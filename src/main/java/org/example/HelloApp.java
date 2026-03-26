0bf04b3ab22f4dff2e3dd3766a9ffb50323ec6f4
harshini1920 <harshinisubramanian1901@gmail.com>
2026-03-26 20:15:08 +0530

    [Harshini] added: Hello with multiple command line

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,14 +1,25 @@
 package org.example;
 public class HelloApp {
+
     public static void main(String[] args) {
-        //UC4
-        String names;
-        if (args.length > 0) {
-            names = String.join(", ", args);
+
+        // UC5
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            names = "World";
+
+            // Use StringBuilder for building names
+            StringBuilder names = new StringBuilder();
+
+            // Enhanced for loop
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+
+            System.out.println("Hello, " + names + "!");
         }
-        System.out.println("Hello, " + names + "!");
     }
 }
-
